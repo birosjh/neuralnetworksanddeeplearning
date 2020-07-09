5dc036d92175708131c28839d79739db88e0ced9
jakebiros <jakebiros@gmail.com>
2020-07-09 22:46:00 +0900

    Add spacing and make all multi-line list comprehensions single line to increase readability

--- network.py
+++ network.py
@@ -108,7 +108,7 @@ class Network(object):
             else:
                 print("Epoch {0} complete".format(epoch))
 
-    
+
     def create_mini_batches(self, num_train, mini_batch_size, training_data) -> list:
 
         mini_batches = []
@@ -130,50 +130,65 @@ class Network(object):
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
         is the learning rate."""
+
         nabla_b = [np.zeros(b.shape) for b in self.biases]
+
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+
         for x, y in mini_batch:
+
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
         to ``self.biases`` and ``self.weights``."""
+
         nabla_b = [np.zeros(b.shape) for b in self.biases]
+
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+
         # feedforward
         activation = x
         activations = [x] # list to store all the activations, layer by layer
+
         zs = [] # list to store all the z vectors, layer by layer
+
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
+
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+
         nabla_b[-1] = delta
+
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
         # Note that the variable l in the loop below is used a little
         # differently to the notation in Chapter 2 of the book.  Here,
         # l = 1 means the last layer of neurons, l = 2 is the
         # second-last layer, and so on.  It's a renumbering of the
         # scheme in the book, used here to take advantage of the fact
         # that Python can use negative indices in lists.
+
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+            
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
